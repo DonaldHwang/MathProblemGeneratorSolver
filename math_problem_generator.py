ebb462f37eac27f78fcdcb4307a97cbbd60d6421
Donald Hwang <949837845@qq.com>
2018-04-18 22:17:33 +0800

    use for to create random number

--- math_problem_generator.py
+++ math_problem_generator.py
@@ -1,48 +1,39 @@
 import random
 
-
 if __name__ == "__main__":
     quiz_file = open("Test.txt", 'w')
 
     operators = ["+", "-", "*", "/"]
     quiz_file.write("If the answer is infinite, your answer should retain two decimal places\n")
     for question_list in range(5):
-        int_1 = random.randint(0, 100)
-        int_2 = random.randint(0, 100)
-        int_3 = random.randint(0, 100)
+        int_list = []
+        for i in range(3):
+            random_int = random.randint(0, 100)
+            int_list.append(random_int)
 
-        fraction_first_number = random.randint(1, 9)
-        fraction_second_number = random.randint(2, 9)
-        while fraction_first_number % fraction_second_number == 0:
+        fraction_list = []
+        for i in range(3):
             fraction_first_number = random.randint(1, 9)
             fraction_second_number = random.randint(2, 9)
-        fraction_1 = "(" + str(fraction_first_number) + "/" + str(fraction_second_number) + ")"
+            while fraction_first_number % fraction_second_number == 0:
+                fraction_first_number = random.randint(1, 9)
+                fraction_second_number = random.randint(2, 9)
+            random_fraction = "(" + str(fraction_first_number) + "/" + str(fraction_second_number) + ")"
+            fraction_list.append(random_fraction)
 
-        fraction_first_number = random.randint(1, 9)
-        fraction_second_number = random.randint(2, 9)
-        while fraction_first_number % fraction_second_number == 0:
-            fraction_first_number = random.randint(1, 9)
-            fraction_second_number = random.randint(2, 9)
-        fraction_2 = "(" + str(fraction_first_number) + "/" + str(fraction_second_number) + ")"
+        decimal_list = []
+        for i in range(3):
+            random_decimal = round(random.random() * 10, 1)
+            decimal_list.append(random_decimal)
 
-        fraction_first_number = random.randint(1, 9)
-        fraction_second_number = random.randint(2, 9)
-        while fraction_first_number % fraction_second_number == 0:
-            fraction_first_number = random.randint(1, 9)
-            fraction_second_number = random.randint(2, 9)
-        fraction_3 = "(" + str(fraction_first_number) + "/" + str(fraction_second_number) + ")"
+        number_list = []
+        for i in range(3):
+            number = random.choice([int_list[i], fraction_list[i], decimal_list[i]])
+            number_list.append(number)
 
-        fix_point_1 = round(random.random() * 10, 1)
-        fix_point_2 = round(random.random() * 10, 1)
-        fix_point_3 = round(random.random() * 10, 1)
-
-        number_1 = random.choice([fraction_1, int_1, fix_point_1])
-        number_2 = random.choice([fraction_2, int_2, fix_point_2])
-        number_3 = random.choice([fraction_3, int_3, fix_point_3])
-
-        problem1 = str(number_1) + " " + operators[random.randint(0, 3)] + " " + str(number_2)
-        problem2 = (str(number_1) + " " + operators[random.randint(0, 3)] + " " + str(number_2) + " "
-                    + operators[random.randint(0, 3)] + " " + str(number_3))
+        problem1 = str(number_list[0]) + " " + operators[random.randint(0, 3)] + " " + str(number_list[1])
+        problem2 = (str(number_list[0]) + " " + operators[random.randint(0, 3)] + " " + str(number_list[1]) + " "
+                    + operators[random.randint(0, 3)] + " " + str(number_list[2]))
         problem = random.choice([problem1, problem2])
         quiz_file.write("Question " + str(question_list + 1) + ": " + problem + " = \n")
 
